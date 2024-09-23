63af4eb436805498672b846e74526720b89c54ec
hosuk <hosuk1497@gmail.com>
2024-09-23 18:51:05 +0900

    update

---
+++ api-orchestration/src/test/java/unit/api/ApiUnitTest.java
@@ -0,0 +1,15 @@
+package unit.api.accountMgt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class ApiUnitTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+}
