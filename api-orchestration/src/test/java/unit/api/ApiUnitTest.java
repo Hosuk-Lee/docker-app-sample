01196ffe622d5186286798f03d500416f19cc205
hosuk <hosuk1497@gmail.com>
2024-09-23 18:51:22 +0900

    update

--- api-orchestration/src/test/java/unit/api/ApiUnitTest.java
+++ api-orchestration/src/test/java/unit/api/ApiUnitTest.java
@@ -1,4 +1,4 @@
-package unit.api.accountMgt;
+package unit.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- api-orchestration/src/test/java/unit/api/accountMgt/계좌관리Controller테스트.java
+++ api-orchestration/src/test/java/unit/api/accountMgt/계좌관리Controller테스트.java
@@ -1,34 +1,32 @@
-package acceptance.accountMgt;
+package unit.api.accountMgt;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import acceptance.ApiAcceptanceTest;
-import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import unit.api.ApiUnitTest;
 
-@DisplayName("계좌관리테스트")
-public class 계좌관리테스트 extends ApiAcceptanceTest {
+
+@DisplayName("계좌관리 Controller 테스트")
+public class 계좌관리Controller테스트 extends ApiUnitTest {
 
     private static String BASE_URL = "api/vq";
 
     @Test
-    void 계좌신규(){
-        assertThat(
-            금리신규(1L)
-        ).isEqualTo(1L);
+    void 계좌신규() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/members/1")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id", Matchers.is(1)));
+        // Add more assertions or actions as needed
+
+
     }
 
-    private long 금리신규(long l) {
-        RestAssured.given().log().all()
-            .basePath(BASE_URL)
-            .body(new Object())
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post()
-            .then()
-            .log().all().extract();
-        return 1L;
-    }
+
 }
